b82664a3f06f1dc920308dfedde57604f80e26b7
ArturMyasnikov <102307516+ArturMyasnikov@users.noreply.github.com>
2022-09-01 13:24:35 +0000

    Deploying to gh-pages from @ ArturMyasnikov/goit-react-hw-05-movies@26eda62d037e968f18025c8ec7e5e098e50bac40 🚀

---
+++ static/js/273.5262f2b0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[273],{1273:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("h1",{style:{textAlign:"center",color:"red"},children:"404 Page not found"})}}}]);
+//# sourceMappingURL=273.5262f2b0.chunk.js.map
