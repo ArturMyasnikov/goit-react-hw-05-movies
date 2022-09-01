a47a5aaa20133437bece2c0d41f107de8993777c
Artur Myasnikov <102307516+ArturMyasnikov@users.noreply.github.com>
2022-09-01 09:16:21 +0300

    hw5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const TrendingFilmsSection = lazy(() => import('./TrandingFilmsSection/TrendingFilmsSection'));
+const TrendingFilmsSection = lazy(() =>
+  import('./TrandingFilmsSection/TrendingFilmsSection')
+);
 const MoviesSection = lazy(() => import('./Movies/MoviesSection'));
 const AboutFilmCard = lazy(() => import('./views/AboutFilmCard'));
 const Layout = lazy(() => import('./views/Layout'));
-
 
 export const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Suspense fallback={<h1 style={{textAlign:'center'}}>Загрузка</h1>}>
+        <Suspense fallback={<h1 style={{ textAlign: 'center' }}>Загрузка</h1>}>
           <Routes>
-            <Route path='/' element={<Layout />}>
+            <Route path="/" element={<Layout />}>
               <Route index element={<TrendingFilmsSection />} />
-              <Route path='/movies/' element={<MoviesSection />} />
-              <Route path='/movies/:filmById/*' element={<AboutFilmCard />} />
-              <Route path='*' element={<Navigate to='/' replace={true} />} />
+              <Route path="/movies/" element={<MoviesSection />} />
+              <Route path="/movies/:filmById/*" element={<AboutFilmCard />} />
+              <Route
+                path="*"
+                element={<div className="wrongUrl">WRONG URL!</div>}
+              />
             </Route>
           </Routes>
         </Suspense>
