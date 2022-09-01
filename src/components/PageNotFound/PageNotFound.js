4aea7e00607203c9556b90afbb195359e340d31d
Artur Myasnikov <102307516+ArturMyasnikov@users.noreply.github.com>
2022-09-01 18:46:43 +0300

    hw5

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,6 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import style from '../Header/Header.module.css';
+
 const PageNotFound = () => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <h1 style={{ textAlign: 'center', color: 'red' }}>404 Page not found</h1>
+    <>
+      <button className={style.btn} onClick={goBack}>
+        Go Back
+      </button>
+      <h1 style={{ textAlign: 'center', color: 'red' }}>404 Page not found</h1>
+    </>
   );
 };
 
